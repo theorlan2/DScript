6b5e89386f97dd806ced5300e4ff94968395482f
Dany Orlando Santos <theorlan2@hotmail.es>
2015-11-01 16:52:03 -0400

    Update and rename dominic.js to DScript.js

---
+++ js/DScript.js
@@ -0,0 +1,380 @@
+/************************** ----------- ******************************  
+			Framework -Dany Orlando Santos Morel -
+ ************************** ----------- ******************************/ 
+
+// --- funciones de selectores individuales ----
+
+// seleciona una clase o id
+function _c(clase) {	return document.querySelector(''+clase);	}
+
+// seleciona por el <tag> <html>
+function _N(clase) {	return document.getElementsByTagName(''+clase);	}
+
+// seleciona por el <tag> <html> y por una clase o id 
+function _All(clase) { 	return document.queryAllSelector(''+clase);	}
+
+
+/*
+----- funcion unica para todas las anteriores (Opcional) ---
+
+function _(clase) {
+if (document.querySelector('.'+clase)!= null){
+
+return document.querySelector('.'+clase);
+
+	} else if(document.querySelectorAll(''+clase)[0]!=null) {
+
+return document.querySelectorAll(''+clase)[0];
+
+	} else {
+
+return document.querySelectorAll('#'+clase);
+
+	}
+}
+*/
+
+
+
+// funcion para media querys css3
+
+/*
+if(m_q({q:"maxw",px:"480px" })) {
+ 
+}else if(m_q({q:"maxw",px:"640px" })) {
+
+}else if(m_q({q:"maxw",px:"768px" })) {
+
+}else if(m_q({types:0,q:"maxw",px:"1024px" })) {
+//	alert("1024px");
+}
+
+*/
+
+
+
+
+
+function m_q(options) {
+ options = options || {};
+var typequery = options.types || ""; 
+var query = options.q || null;
+var valor = options.px || null;
+var result = options.result || null;
+var tipos = new Array("all","screen","tv","aural","braille","embossed","handheld","print","projection","speech","tty");
+
+if (!isNaN(typequery) && typequery == ""  ) {
+typequery = tipos[typequery];
+
+// alert("true");
+}
+
+switch(query) {
+	case "minw":
+		query = "min-width::";		
+		break;
+	case "maxw":
+		query = "max-width:";		
+		break;
+	case "maxh":
+		query = "max-height:";		
+		break;
+	case "minh":
+		query = "min-height:";		
+		break;
+	case "mindw":
+		query = "min-device-width:";		
+		break;
+	case "maxr":
+		query = "max-resolution:";		
+		break;
+	default :
+		query = query;
+		break;
+	}
+if (typequery == "" || typeof(typequery) == "undefined") {
+typequery == "";  
+query = "("+query+valor+")";
+}else {
+query = typequery+" ("+query+" "+valor+")";
+}
+
+
+//alert(typequery+" r: "+ query+" r: "+ valor+" r: "+ result);
+	if ( query != null) {
+ 
+ 	 	if(window.matchMedia(query).matches){
+			if (result == null) {
+	return true;	
+				}else{
+					return false;
+			}
+		}else{
+					return false;	
+		}
+	}
+}
+
+ 
+
+var _cc = function (clase,contexto)  {  return new _cc.inic(clase,contexto); 	 }
+/*var _c = function(clase) {	return document.querySelector('.'+clase);};*/
+var version = "0.1.0";
+ 
+inic  =  _cc.inic = function (clase,contexto,indic) {
+// Metodo selector del objeto
+this.clase = clase,
+this.contexto = contexto,
+this.indic = indic,
+
+selectores = function(clase){ 
+
+selector = document.querySelector(clase);
+
+},
+ 
+constructor = function(select,contexto){
+selectores(select);
+ },
+constructor(this.clase,this.contexto);
+}
+
+
+// Metodo transformacion, para transformar el objeto
+inic.prototype.transfomar = function (detalles,clase){
+//selectores(clase);
+(typeof(selector.style.webkitTransform) != "undefined") ? selector.style.webkitTransform = ''+detalles : selector.style["webkit-transform"] = [''+detalles];
+(typeof(selector.style.MozTransform) != "undefined") ? selector.style.MozTransform = ''+detalles : selector.style["moz-Transform"] = [''+detalles];
+(typeof(selector.style.msTransform) != "undefined") ? selector.style.msTransform = ''+detalles : selector.style["ms-transform"] = [''+detalles];
+(typeof(selector.style.OTransform) != "undefined") ? selector.style.OTransform = ''+detalles : selector.style["o-transform"] = [''+detalles];
+(typeof(selector.style.transform) != "undefined") ? selector.style.transform = ''+detalles : selector.style["transform"] = [''+detalles];
+	}
+
+// Metodo para la transicion
+inic.prototype.transicion = function (detalles,clase){
+//selectores(clase);
+(typeof(selector.style.webkitTransition) != "undefined" ) ? selector.style.webkitTransition = ''+detalles : selector.style["webkit-transition"] = [''+detalles];
+(typeof(selector.style.MozTransition) != "undefined" ) ? selector.style.MozTransition = ''+detalles : selector.style["noz-transition"] = [''+detalles];
+(typeof(selector.style.msTransition) != "undefined" ) ? selector.style.msTransition = ''+detalles : selector.style["ms-transition"] = [''+detalles];
+(typeof(selector.style.OTransition) != "undefined" ) ? selector.style.OTransition = ''+detalles : selector.style["o-transition"] = [''+detalles];
+(typeof(selector.style.transition) != "undefined" ) ? selector.style.transition = ''+detalles : selector.style["transition"] = [''+detalles];
+	}
+
+inic.prototype.tipodetiempo = function (detalles,clase){
+//selectores(clase);
+(typeof(selector.style.webkitAnimationTimingFunction) != "undefined") ?  selector.style.webkitAnimationTimingFunction = ''+detalles : selector.style["webkitAnimationTimingFunction"] = [''+detalles];
+(typeof(selector.style.MozAnimationTimingFunction) != "undefined") ?  selector.style.MozAnimationTimingFunction = ''+detalles : selector.style["MozAnimationTimingFunction"] = [''+detalles];
+(typeof(selector.style.msAnimationTimingFunction) != "undefined") ?  selector.style.msAnimationTimingFunction = ''+detalles : selector.style["msAnimationTimingFunction"] = [''+detalles];
+(typeof(selector.style.AnimationTimingFunction) != "undefined") ?  selector.style.AnimationTimingFunction = ''+detalles : selector.style["AnimationTimingFunction"] = [''+detalles];
+	}
+
+inic.prototype.css = function(css,valor) {
+ 
+selector.style[css] = [valor] ;
+
+};
+
+inic.prototype.escribir = function(valor) {
+alert(selector);
+selector.innerHTML = valor;
+
+}
+
+inic.prototype.G_Maps = function(opcion) {
+var lat = opcion.lat || 18.4643133
+,lng = opcion.lng || -69.8771667
+,z = opcion.z || 8
+,scroll = opcion.scroll || true
+,estilos = opcion.estilos || null
+,tipo_mapa = opcion.tipo || "road"
+,direcciones = opcion.direcciones || false
+,dir1 = opcion.dir1 || null
+,dir2 = opcion.dir2 || null
+,rotar = opcion.rotar || 0;
+ G_Maps({
+div:this.clase,
+lat:lat,
+lng: lng,
+z: z,
+scroll: scroll,
+estilos: estilos,
+tipo: tipo_mapa,
+direcciones: direcciones,
+dir1: dir1,
+dir2: dir2,
+rotar: rotar,
+ })
+}
+
+inic.prototype._ajaxs = function(opciones) {
+var url = opciones.url || null;
+var file = opciones.file || null;
+var fin = opciones.fin || null;
+var resq = opciones.resq || null;
+_ajaxs({
+	url:url,
+	file:file,
+	classn:this.clase,
+	resq:resq,
+	fin:fin
+
+});
+
+}
+/* Funciones Localstorage  */
+var lcst = {
+
+// Guardar Datos en el Localstorage
+guardar : function(key,dato){
+ 
+	localStorage.setItem(key,dato);
+
+},
+// ver Dato del Localstorage
+verdatos : function (key){
+
+	return localStorage.getItem(key);
+
+},
+
+// Eliminar Dato del Localstorage
+eliminar: function (key){
+
+	localStorage.removeItem(key);
+
+	}
+}
+
+function  _ajaxs(opciones) {
+this.url = opciones.url || "";
+this.file = opciones.file || null;
+selec = opciones.classn || null;
+this.response = opciones.response || null;
+succes = opciones.fin || null;
+var resq = opciones.resq || null;
+this.respuesta = "";
+
+(this.url[0] != "?" && resq == "GET"   ) ? this.url =  "?"+this.url : this.url;
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function (fin) {
+ 
+	if (xhttp.readyState == 4 && xhttp.status == 200 ) {
+	this.respuesta = xhttp.responseText;
+
+if ( selec != null && selec != ""  ) {  
+	_cc(""+selec).escribir(this.respuesta);
+}
+
+if (succes && typeof(succes) === "function") {
+        succes(xhttp.responseText);
+ 
+    }
+	}
+}
+
+	if (resq == "POST" ) {
+  xhttp.open("POST",this.file,true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send(this.url);
+	}else {
+  xhttp.open("GET",this.file+this.url,true);
+  xhttp.send();
+	}
+}
+
+
+// Google Maps Api  
+function G_Maps(opcion){ 
+var select = opcion.div || null
+,lat = opcion.lat || 18.4643133
+,lng = opcion.lng || -69.8771667
+,z = opcion.z || 8
+,scroll = opcion.scroll || true
+,estilos = opcion.estilos || null
+,tipo_mapa = opcion.tipo || "road"
+,direcciones = opcion.direcciones || false
+,dir1 = opcion.dir1 || null
+,dir2 = opcion.dir2 || null
+,rotar = opcion.rotar || 0;
+rotacion = false;
+switch(tipo_mapa) {
+	case "road":
+ tipo_mapa = google.maps.MapTypeId.ROADMAP;
+		break;
+	case "satelite":
+ tipo_mapa = google.maps.MapTypeId.SATELLITE;
+		break;
+	case "hibrido":
+ tipo_mapa = google.maps.MapTypeId.HYBRID;
+rotacion = true;
+		break;
+	case "terreno":
+ tipo_mapa = google.maps.MapTypeId.TERRAIN;
+rotacion = true;
+		break;
+}
+
+if (direcciones != true ) {
+ var map;
+  map = new google.maps.Map(_c(select), {
+    center: {lat: lat, lng: lng},
+    zoom: z,
+    scrollwheel:scroll,
+    styles: estilos,
+    mapTypeId: tipo_mapa
+  });
+}
+if(rotacion){
+map.setTilt(rotar);
+}
+
+if (direcciones == true ) {
+ 
+ var map;
+  map = new google.maps.Map(_c(select), {
+    center: dir1,
+    zoom: z,
+    scrollwheel:scroll,
+    styles: estilos,
+    mapTypeId: tipo_mapa
+  });
+ 
+  var directionsDisplay = new google.maps.DirectionsRenderer({
+    map: map
+  });
+
+  var request = {
+    destination: dir2,
+    origin: dir1,
+    travelMode: google.maps.TravelMode.DRIVING
+  };
+  // Set destination, origin and travel mode.
+
+  // Pass the directions request to the directions service.
+  var directionsService = new google.maps.DirectionsService();
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      // Display the route on the map.
+      directionsDisplay.setDirections(response);
+    }
+  }); 
+
+}
+ 
+G_Maps.set = function(opciones) {
+ var center = opciones.centro || null 
+	if (center != null) {
+	 map.setCenter({lat: -34, lng: 151});
+		}
+	}
+}
+
+Leer Json 
+var _json = {
+leer : function(data) {
+return JSON.parse(data); 
+}
+
+}
+
