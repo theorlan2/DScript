44fc336ca4e30bde50508ba431ebc29f3de959e1
Dany Orlando Santos <theorlan2@hotmail.es>
2015-11-21 18:22:47 -0400

    Update DScript.js

--- js/DScript.js
+++ js/DScript.js
@@ -492,3 +492,36 @@ return winW,winH;
 }
 
 
+  function findPosX(obj)
+  {
+    var curleft = 0;
+    if(obj.offsetParent)
+        while(1) 
+        {
+          curleft += obj.offsetLeft;
+          if(!obj.offsetParent)
+            break;
+          obj = obj.offsetParent;
+        }
+    else if(obj.x)
+        curleft += obj.x;
+    return curleft;
+  }
+
+  function findPosY(obj)
+  {
+    var curtop = 0;
+    if(obj.offsetParent)
+        while(1)
+        {
+          curtop += obj.offsetTop;
+          if(!obj.offsetParent)
+            break;
+          obj = obj.offsetParent;
+        }
+    else if(obj.y)
+        curtop += obj.y;
+    alert(curtop);
+    return curtop;
+  }
+
